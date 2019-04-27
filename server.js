6229e2fa3259b5954fa53b1139485fd910bc17b9
Austin Shymanski <austin.shymanski@gmail.com>
2019-04-27 17:43:15 -0400

    Problem connecting to the db

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const path = require('path')
 const home = require('./routes/home')
+const mongoose = require('mongoose')
 const register = require('./routes/register') //Import the register route
+const { MongoClient } = require("mongodb")
+const uri = 'mongodb://localhost:5000';
+
+(async function()	{
+	try {
+		const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+
+		client.close();
+	} catch(e) {
+		console.error(e)
+	}
+})
 
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
@@ -15,3 +28,4 @@ app.use('/register', register) //Connect the path and route
 
 app.listen(5000)
 console.log('App running on http://localhost:5000')
+
